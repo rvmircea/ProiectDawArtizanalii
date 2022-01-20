ce6afb39576375319cb8b050a0251cb3ba22fcb6
rvmircea <vmircea1771@yahoo.com>
2022-01-20 17:41:06 +0200

    First Commit Via Upload
    Implementat Modele
    DbContext + Controllere pentru Users și Produse

---
+++ Startup.cs
@@ -0,0 +1,63 @@
+using Artizanalii.Data;
+using Artizanalii.Interfaces;
+using Artizanalii.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Artizanalii
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+
+                );
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IProdusRepository, ProdusRepository>();
+            services.AddDbContext<ArtizanaliiContext>(opt =>
+            opt.UseSqlServer("Data Source=DESKTOP-I7UGUCL;Initial Catalog=DbArtizanalii;Integrated Security=True")
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
